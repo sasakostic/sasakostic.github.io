542fe9e2f1dbd93a956085d834387aa13062b8a7
sasakostic <3274823+sasakostic@users.noreply.github.com>
2025-10-07 01:33:11 +0200

    project name

--- src/templates/ProjectsList.tsx
+++ src/templates/ProjectsList.tsx
@@ -16,7 +16,7 @@ const ProjectsList = ({ pageContext }: PageProps<Record<string, never>, Projects
       <ul>
         {projects.map((project) => (
           <li key={project.id}>
-            <Link to={`/projects/${project.id}`}>{project.title}</Link>
+            <Link to={`/projects/${project.id}`}>{project.name}</Link>
           </li>
         ))}
       </ul>
