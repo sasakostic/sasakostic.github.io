8aa4559c4b7f413c8db2d734cb13c77cd7dad3ef
sasakostic <3274823+sasakostic@users.noreply.github.com>
2025-10-07 01:22:00 +0200

    Indentation

--- src/templates/ProjectsList.tsx
+++ src/templates/ProjectsList.tsx
@@ -3,24 +3,24 @@ import { Link, PageProps } from 'gatsby';
 import { Project } from '../types/Project';
 
 interface ProjectsListContext {
-    projects: Project[];
+  projects: Project[];
 }
 
 const ProjectsList: React.FC<PageProps<{}, ProjectsListContext>> = ({ pageContext }) => {
-    const { projects } = pageContext;
+  const { projects } = pageContext;
 
-    return (
-        <main className="projects-list">
-            <h1>Projects</h1>
-            <ul>
-                {projects.map((project) => (
-                    <li key={project.id}>
-                        <Link to={`/projects/${project.id}`}>{project.title}</Link>
-                    </li>
-                ))}
-            </ul>
-        </main>
-    );
+  return (
+    <main className="projects-list">
+      <h1>Projects</h1>
+      <ul>
+        {projects.map((project) => (
+          <li key={project.id}>
+            <Link to={`/projects/${project.id}`}>{project.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 };
 
 export default ProjectsList;
