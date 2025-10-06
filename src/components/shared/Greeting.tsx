bf69e303f14f0a58696a60a5794843ebbb934f36
sasakostic <3274823+sasakostic@users.noreply.github.com>
2025-10-06 13:18:46 +0200

    Fixed linting errors

--- src/components/shared/Greeting.tsx
+++ src/components/shared/Greeting.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
-// import HyperLink from './HyperLink';
-import type { Link } from '../../types/Link';
-import { routes } from '../../constants/routes';
 
 const Greeting = (): React.ReactElement => {
-
-
   return (
     <p className="font-light">
       Hi there! I&apos;m Sasa, a software developer. This part will be edited.
